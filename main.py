1f0956fca86d22464e50d70e9d364ead619ce0d5
brazafonso <61985783+brazafonso@users.noreply.github.com>
2023-05-19 17:10:23 +0100

    limpar main

--- main.py
+++ main.py
@@ -18,6 +18,8 @@ import json
 import argparse
 
 # Default config
+USER = ""
+PASSWORD = ""
 max_queue = 5
 max_planes = 5
 spawn_time = 2
@@ -26,9 +28,9 @@ plane_speed = 10
 max_wait_in_station = 60
 max_wait_landing = 60
 max_wait_take_off = 60
-origin='Porto'
-destination='Lisboa'
-airlines_list = []
+origin_list=['Porto','Lisboa']
+destination_list=['Porto','Lisboa']
+airlines_list = ["Tap"]
 
 def parse_arguments()->argparse.Namespace:
     """Process arguments from stdin"""
@@ -46,14 +48,46 @@ def parse_arguments()->argparse.Namespace:
     parser.add_argument('-a','--auction'                                                                                           ,action='store_true'        ,help='Activate auction.')
     return parser.parse_args()
 
-def get_airline_name():
-    '''Escolhe aleatoriamente uma empresa da lista de empresas'''
-    global airlines_list
+def load_config():
+    '''Loads values from configuration file'''
+    global max_queue,max_planes,spawn_time
+    global dash_board_period,plane_speed,max_wait_in_station
+    global max_wait_landing,max_wait_take_off,origin_list
+    global destination_list,airlines_list
+    if 'max_queue' in config:
+        max_queue = config['max_queue']
+    if 'max_planes' in config:
+        max_planes = config['max_planes']
+    if 'spawn_time' in config:
+        spawn_time = config['spawn_time']
+    if 'dash_board_period' in config:
+        dash_board_period = config["dash_board_period"]
+    if 'plane_speed' in config:
+        plane_speed = config["plane_speed"]
+    if 'max_wait_in_station' in config:
+        max_wait_in_station = config["max_wait_in_station"]
+    if 'max_wait_landing' in config:
+        max_wait_landing = config["max_wait_landing"]
+    if 'max_wait_take_off' in config:
+        max_wait_take_off = config["max_wait_take_off"]
+    if 'origin' in config:
+        origin_list = config["origin"]
+    if 'destination' in config:
+        destination_list = config["destination"]
+    if 'airlines' in config:
+        airlines_conf = config["airlines"]
+        airlines_list = list(airlines_conf.keys())
 
-    airline_name = None
-    if airlines_list:
-        airline_name = airlines_list[random.randint(0,len(airlines_list)-1)]
-    return airline_name
+
+
+
+def choose_rand(list:list):
+    '''Escolhe aleatoriamente uma empresa da lista de empresas'''
+
+    chosen = None
+    if list:
+        chosen = list[random.randint(0,len(list)-1)]
+    return chosen
 
 def get_plane_type():
     '''Escolhe aleatoriamente um tipo de aviao'''
@@ -64,6 +98,7 @@ def get_plane_type():
 
 def create_plane_agents(n_planes,control_tower,station_manager,airport_map:AirportMap,logs,logs_file):
     '''Cria a lista de agentes aviao, alguns em gares e outros no ar e atualiza mapa do aeroporto'''
+    airport_name = airport_map.name
     planes_ground = []
     planes_air = []
     plane_id = 0
@@ -78,10 +113,11 @@ def create_plane_agents(n_planes,control_tower,station_manager,airport_map:Airpo
             assigned = 0
             for station in dict.values():
                 airline_name = station.airline_name
+                destination = choose_rand(destination_list)
                 plane_agent = PlaneAgent(f'plane{plane_id}@{USER}',PASSWORD,state=False,airline_name=airline_name,
                                    type=type,plane_speed=plane_speed,max_wait_in_station=max_wait_in_station,
                                    max_wait_landing=max_wait_landing,max_wait_take_off=max_wait_take_off,
-                                   origin=origin,destination=destination)
+                                   origin=airport_name,destination=destination)
                 plane_agent.set('id',plane_id)
                 plane_agent.set('control_tower',control_tower)
                 plane_agent.set('station_manager',station_manager)
@@ -99,12 +135,13 @@ def create_plane_agents(n_planes,control_tower,station_manager,airport_map:Airpo
     # Criar avioes no ar
     n_planes -= len(planes_ground)
     for _ in range(0,n_planes):
-        airline_name = get_airline_name()
+        airline_name = choose_rand(airlines_list)
+        origin = choose_rand(origin_list)
         type = get_plane_type()
         plane_agent = PlaneAgent(f'plane{plane_id}@{USER}',PASSWORD,state=True,airline_name=airline_name,
                                    type=type,plane_speed=plane_speed,max_wait_in_station=max_wait_in_station,
                                    max_wait_landing=max_wait_landing,max_wait_take_off=max_wait_take_off,
-                                   origin=origin,destination=destination)
+                                   origin=origin,destination=airport_name)
         plane_agent.set('id',plane_id)
         plane_agent.set('control_tower',control_tower)
         plane_agent.set('station_manager',station_manager)
@@ -114,6 +151,119 @@ def create_plane_agents(n_planes,control_tower,station_manager,airport_map:Airpo
         planes_air.append(plane_agent)
 
     return (planes_ground,planes_air,airport_map)
+
+
+
+def draw_map(airport_map:AirportMap):
+    '''Desenha aeroporto'''
+    airport_map.set_frame()
+    airport_map.place_airstrips()
+    airport_map.place_stations()
+    airport_map.place_roads()
+    airport_map.map_update_stations(airport_map.get_stations())
+    airport_map.draw_map()
+
+
+def create_control_tower(args,airport_map:AirportMap,CT,SM):
+    '''Cria um agente torre de controlo'''
+    control_tower = ControlTowerAgent(CT,PASSWORD)
+    control_tower.set('airport_map',airport_map.get_copy())
+    control_tower.set('max_queue',max_queue)
+    control_tower.set('n_planes',n_planes)
+    control_tower.set('station_manager',SM)
+    control_tower.set('logs',args.logs)
+    control_tower.set('logs_file',args.logs_file)
+    return control_tower
+
+
+def create_station_manager(args,airport_map:AirportMap,SM,CT):
+    '''Cria um agente gestor de gares'''
+    station_manager = StationManagerAgent(SM,PASSWORD)
+    station_manager.set('airport_map',airport_map.get_copy())
+    station_manager.set('control_tower',CT)
+    station_manager.set('logs',args.logs)
+    station_manager.set('logs_file',args.logs_file)
+    return station_manager
+
+def create_auction_manager(args,airport_map:AirportMap,AM,SM):
+    '''Cria um agente gestor de leiloes'''
+    auction_manager = AuctionManagerAgent(AM,PASSWORD)
+    auction_manager.set('airport_map',airport_map.get_copy())
+    auction_manager.set('station_manager',SM)
+    auction_manager.set('logs',args.logs)
+    auction_manager.set('logs_file',args.logs_file)
+    return auction_manager
+
+def create_airline_agent(args,AM,airline_jid,airline_obj):
+    '''Cria um agente de companhia aerea'''
+    airline_agent = AirlineAgent(airline_jid, PASSWORD, airline=airline_obj)
+    airline_agent.set('auction_manager',AM)
+    airline_agent.set('logs',args.logs)
+    airline_agent.set('logs_file',args.logs_file)
+    return airline_agent
+
+
+def create_dashboard_manager(args,airport_map:AirportMap,DM,CT):
+    '''Cria um agente dashboard manager'''
+    dashboard_manager = Dashboard_Manager(DM,PASSWORD,period=dash_board_period)
+    dashboard_manager.set('airport_map',airport_map)
+    dashboard_manager.set('control_tower',CT)
+    dashboard_manager.set('logs',args.logs)
+    dashboard_manager.set('logs_file',args.logs_file)
+    return dashboard_manager
+
+
+def start_auctions(args,airport_map,airlines_conf,AM,SM):
+    '''Trata de iniciar a funcionalidade de leiloes'''
+    # Start auction manager
+    auction_manager = create_auction_manager(args,airport_map,AM,SM)
+    futureAM = auction_manager.start()
+
+    # Start airline agents
+    future_airlines = []
+    
+    for airline_name in airlines_list:
+        airline_jid = ('airline_'+str(airline_name)+'@'+USER).lower()
+        type = airlines_conf[airline_name]["type"]
+        budget = airlines_conf[airline_name]["budget"]
+        costs = airlines_conf[airline_name]["costs"]
+        profit_margin = airlines_conf[airline_name]["profit_margin"]
+        strategy = airlines_conf[airline_name]["strategy"]
+        airline_obj = Airline(airline_jid, airline_name, type, budget, costs, profit_margin, strategy)
+        
+        airline_agent = create_airline_agent(args,AM,airline_jid,airline_obj)
+
+        future_airlines.append(airline_agent.start())
+        
+    futureAM.result()
+
+    for future in future_airlines:
+        future.result()
+
+
+def start_planes(ground_plane_agents,air_plane_agents):
+    '''Trata de iniciar os agentes aviao'''
+    futureP = []
+    # Iniciar agentes aviao no chao
+    for plane in ground_plane_agents:
+        futureP.append(plane.start())
+
+
+    time.sleep(spawn_time)
+    # Iniciar agentes aviao no ar (delay de spawn)
+    for plane in air_plane_agents:
+        futureP.append(plane.start())
+        time.sleep(spawn_time)
+
+    for future in futureP:
+        future.result()
+
+
+
+#--------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+#--------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+#--------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
 
 # Main
 if __name__ == "__main__":
@@ -141,136 +291,49 @@ if __name__ == "__main__":
                 CT = f'control_tower@{USER}'
                 SM = f'station_manager@{USER}'
                 AM = f'auction_manager@{USER}'
+                DM = f'dashboard_manager@{USER}'
 
-                ## airport map
+                # airport map
                 airport_map = AirportMap(config)
 
-                ## airport config variables
-                if 'max_queue' in config:
-                    max_queue = config['max_queue']
-                if 'max_planes' in config:
-                    max_planes = config['max_planes']
-                if 'spawn_time' in config:
-                    spawn_time = config['spawn_time']
-                if 'dash_board_period' in config:
-                    dash_board_period = config["dash_board_period"]
-                if 'plane_speed' in config:
-                    plane_speed = config["plane_speed"]
-                if 'max_wait_in_station' in config:
-                    max_wait_in_station = config["max_wait_in_station"]
-                if 'max_wait_landing' in config:
-                    max_wait_landing = config["max_wait_landing"]
-                if 'max_wait_take_off' in config:
-                    max_wait_take_off = config["max_wait_take_off"]
-                if 'origin' in config:
-                    origin = config["origin"]
-                if 'destination' in config:
-                    destination = config["destination"]
-                if 'airlines' in config:
-                    airlines_conf = config["airlines"]
-                    airlines_list = list(airlines_conf.keys())
+                # Ler configuracoes utilizadas
+                load_config()
 
                 # Criar avioes (devido aos que começam nas gares)
                 n_planes = random.randint(1,max_planes)
                 ground_plane_agents,air_plane_agents,airport_map = create_plane_agents(n_planes,CT,SM,airport_map,args.logs,args.logs_file)
                 print('Info:',len(ground_plane_agents),len(air_plane_agents),n_planes)
-
-
-                # Desenha aeroporto
-                airport_map.set_frame()
-                airport_map.place_airstrips()
-                airport_map.place_stations()
-                airport_map.place_roads()
-                airport_map.map_update_stations(airport_map.get_stations())
-                airport_map.draw_map()
                 
-                # Criar torre de controlo
-                control_tower = ControlTowerAgent(CT,PASSWORD)
-                control_tower.set('airport_map',airport_map.get_copy())
-                control_tower.set('max_queue',max_queue)
-                control_tower.set('n_planes',n_planes)
-                control_tower.set('station_manager',SM)
-                control_tower.set('logs',args.logs)
-                control_tower.set('logs_file',args.logs_file)
+                # Desenha aeroporto
+                draw_map(airport_map)
 
+
+                # Criar torre de controlo
+                control_tower = create_control_tower(args,airport_map,CT,SM)
                 futureCT = control_tower.start()
 
                 # Criar station manager
-                station_manager = StationManagerAgent(SM,PASSWORD)
-                station_manager.set('airport_map',airport_map.get_copy())
-                station_manager.set('control_tower',CT)
-                station_manager.set('logs',args.logs)
-                station_manager.set('logs_file',args.logs_file)
-
+                station_manager = create_station_manager(args,airport_map,SM,CT)
                 futureSM = station_manager.start()
-
 
 
                 futureCT.result()
                 futureSM.result()
 
+                # Ativar leilões
                 if args.auction:
-                    print('Auction functionality activated...')
+                    airlines_conf = config["airlines"]
+                    start_auctions(args,airport_map,airlines_conf,AM,SM)
 
-                    # Start auction manager
-                    auction_manager = AuctionManagerAgent(AM,PASSWORD)
-                    auction_manager.set('airport_map',airport_map.get_copy())
-                    auction_manager.set('station_manager',SM)
-                    auction_manager.set('logs',args.logs)
-                    auction_manager.set('logs_file',args.logs_file)
-                    
-                    futureAM = auction_manager.start()
-
-                    # Start airline agents
-                    future_airlines = []
-                    for airline_name in airlines_list:
-                        airline_jid = ('airline_'+str(airline_name)+'@'+USER).lower()
-                        type = airlines_conf[airline_name]["type"]
-                        budget = airlines_conf[airline_name]["budget"]
-                        costs = airlines_conf[airline_name]["costs"]
-                        profit_margin = airlines_conf[airline_name]["profit_margin"]
-                        strategy = airlines_conf[airline_name]["strategy"]
-                        airline_obj = Airline(airline_jid, airline_name, type, budget, costs, profit_margin, strategy)
-                        
-                        airline_agent = AirlineAgent(airline_jid, PASSWORD, airline=airline_obj)
-                        airline_agent.set('auction_manager',AM)
-                        airline_agent.set('logs',args.logs)
-                        airline_agent.set('logs_file',args.logs_file)
-
-                        future_airlines.append(airline_agent.start())
-                        
-                    futureAM.result()
-
-                    for future in future_airlines:
-                        future.result()
 
                 
-
-                            
                 # Criar Gestor de Dashboards
-                dashboard_manager = Dashboard_Manager(f'dashboard_manager@{USER}',PASSWORD,period=dash_board_period)
-                dashboard_manager.set('airport_map',airport_map)
-                dashboard_manager.set('control_tower',CT)
-                dashboard_manager.set('logs',args.logs)
-                dashboard_manager.set('logs_file',args.logs_file)
-
+                dashboard_manager = create_dashboard_manager(args,airport_map,DM,CT)
                 futureDM = dashboard_manager.start()
                 futureDM.result()
 
-                futureP = []
-                # Iniciar agentes aviao no chao
-                for plane in ground_plane_agents:
-                    futureP.append(plane.start())
-
-
-                time.sleep(spawn_time)
-                # Iniciar agentes aviao no ar (delay de spawn)
-                for plane in air_plane_agents:
-                    futureP.append(plane.start())
-                    time.sleep(spawn_time)
-
-                for future in futureP:
-                    future.result()
+                # Iniciar agentes aviao
+                start_planes(ground_plane_agents,air_plane_agents)
 
 
                 # Esperar ate torre desligar
